6ec9ad8df2edb82821e5d38cf7c415449e344ffd
dmaqueda <dmaqueda05@gmail.com>
2026-05-26 11:54:02 +0200

    Delete Proyectos directory

--- Proyectos/backend/src/ProyectosInnova.Application/Projects/IProjectService.cs
+++
@@ -1,9 +0,0 @@
-namespace ProyectosInnova.Application.Projects;
-
-public interface IProjectService
-{
-    Task<IReadOnlyList<ProjectDto>> GetProjectsAsync(ProjectQuery query, CancellationToken cancellationToken);
-
-    Task<ProjectDto?> GetByIdAsync(int id, CancellationToken cancellationToken);
-}
-
